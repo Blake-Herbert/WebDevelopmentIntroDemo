fb8daee2a8374cffc982ddf8fe4e67e7f8f51334
Blake <bwherb1@gmail.com>
2025-01-23 17:52:45 -0500

    Added loops to script

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 function toggle() {
-    const container1 = document.getElementById("container");
+    const container1 = document.getElementById("container1");
     const container2 = document.getElementById("container2");
     const container3 = document.getElementById("container3");
-    const bigContainer = document.getElementById("bigContainer");
+
+    const containers = [container1, container2, container3];
+
+    containers.forEach(container => {
+        container.classList.toggle("container");
+    });
     
-    container1.classList.toggle("container");
-    container2.classList.toggle("container");
-    container3.classList.toggle("container");
+    const bigContainer = document.getElementById("bigContainer");
     bigContainer.classList.toggle("bigContainer");
 
-    const button1 = document.getElementById("button1");
-    const button2 = document.getElementById("button2");
-    const button3 = document.getElementById("button3");
+    const buttons = document.querySelectorAll("button");
 
-    button1.classList.toggle("button");
-    button2.classList.toggle("button");
-    button3.classList.toggle("button");
-
+    buttons.forEach(button => {
+        button.classList.toggle("button");
+    });
 
     console.log("Hello World!");
 }
+
+toggle();
