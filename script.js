13bbf7a3f79d5ee928fa56b8a58239700747a24e
Blake-Herbert <bwherb1@gmail.com>
2025-03-12 16:15:36 -0400

    Removed repetitive code and changed max width

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
 function toggle() {
-    const container1 = document.getElementById("container1");
-    const container2 = document.getElementById("container2");
-    const container3 = document.getElementById("container3");
-
-    const containers = [container1, container2, container3];
+    const body = document.querySelector("body");
+    const containers = document.querySelectorAll(".container");
+    const buttons = document.querySelectorAll("button");
+    
+    body.classList.toggle("active");
 
     containers.forEach(container => {
-        container.classList.toggle("container");
+        container.classList.toggle("active");
     });
-    
-    const bigContainer = document.getElementById("bigContainer");
-    bigContainer.classList.toggle("bigContainer");
-
-    const buttons = document.querySelectorAll("button");
 
     buttons.forEach(button => {
-        button.classList.toggle("button");
+        button.classList.toggle("active");
     });
 
-    console.log("Hello World!");
+    console.log("Hello World!"); // This is here to demonstrate the browser dev tools console
 }
-
-toggle();
